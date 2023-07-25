9d00fa0c56972eb6f9ed588a7e732dc55d0ad8f2
3steban99 <barrionuevo99@outlook.com>
2023-07-25 14:53:38 -0300

    creacion y prueba de api en vercel

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const mysql = require('mysql')
+const myconn = require('express-myconnection')
+const cors = require('cors')
+require('dotenv').config();
+
+
+const app = express()
+
+app.set('port', process.env.PORT || 3001)
+
+const dbOptions={
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
+}
+
+app.use(myconn(mysql, dbOptions, 'single'))
+app.use(express.json())
+app.use(cors())
+
+
+app.get('/', (req, res)=>{
+    res.send('Bienvenido a mi API')
+});
+
+
+
+app.get('/Productos', (req, res)=>{
+    req.getConnection((err, conn)=>{
+        if(err) return res.send(err)
+
+        conn.query('SELECT * FROM productos', (err, rows)=>{
+            if(err) return res.send(err)
+            res.json(rows)
+        })
+    })
+})
+
+app.listen(app.get('port'), ()=>{
+    console.log('server corriendo en el puerto', app.get('port'))
+})
+
+module.exports = app;
