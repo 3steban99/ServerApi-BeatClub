a9d1b358c615ac21b60f6678ebde05ff2b5718ce
3steban99 <barrionuevo99@outlook.com>
2023-07-31 14:05:03 -0300

    prueba de ruta para modificar

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express()
 
 app.set('port', process.env.PORT || 3001)
 
-const dbOptions={
+const dbOptions = {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     user: process.env.DB_USER,
@@ -22,43 +22,47 @@ app.use(express.json())
 app.use(cors())
 
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.send('Bienvenido a mi API')
 });
 
 
 //ruta para todos los productos del menu
 
-app.get('/Productos', (req, res)=>{
-    req.getConnection((err, conn)=>{
-        if(err) return res.send(err)
+app.get('/Productos', (req, res) => {
+    req.getConnection((err, conn) => {
+        if (err) return res.send(err)
 
-        conn.query('SELECT p.producto_id, p.nombre, p.precio, p.descripcion, c.nombre AS categoria_nombre FROM productos p LEFT JOIN categorias c ON p.categoria_id = c.categoria_id ORDER BY c.categoria_id', (err, rows)=>{
-            if(err) return res.send(err)
+        conn.query('SELECT p.producto_id, p.nombre, p.precio, p.descripcion, c.nombre AS categoria_nombre FROM productos p LEFT JOIN categorias c ON p.categoria_id = c.categoria_id ORDER BY c.categoria_id', (err, rows) => {
+            if (err) return res.send(err)
             res.json(rows)
         })
     })
 })
 
-//ruta para la categoria vino
 
-app.get('/Productos/vino', (req, res)=>{
-    req.getConnection((err, conn)=>{
-        if(err) return res.send(err)
+//RUTAS PARA EL MENU
 
-        conn.query('SELECT * FROM productos WHERE categoria_id = 1', (err, rows)=>{
-            if(err) return res.send(err)
+//ruta categoria vino
+
+app.get('/Productos/vino', (req, res) => {
+    req.getConnection((err, conn) => {
+        if (err) return res.send(err)
+
+        conn.query('SELECT * FROM productos WHERE categoria_id = 1', (err, rows) => {
+            if (err) return res.send(err)
             res.json(rows)
         })
     })
 })
 
-app.get('/Productos/espumante', (req, res)=>{
-    req.getConnection((err, conn)=>{
-        if(err) return res.send(err)
+//ruta categoria espumante
+app.get('/Productos/espumante', (req, res) => {
+    req.getConnection((err, conn) => {
+        if (err) return res.send(err)
 
-        conn.query('SELECT * FROM productos WHERE categoria_id = 2', (err, rows)=>{
-            if(err) return res.send(err)
+        conn.query('SELECT * FROM productos WHERE categoria_id = 2', (err, rows) => {
+            if (err) return res.send(err)
             res.json(rows)
         })
     })
@@ -68,26 +72,28 @@ app.get('/Productos/espumante', (req, res)=>{
 
 //ruta de categorias
 
-app.get('/Categorias', (req, res)=>{
-    req.getConnection((err, conn)=>{
-        if(err) return res.send(err)
+app.get('/Categorias', (req, res) => {
+    req.getConnection((err, conn) => {
+        if (err) return res.send(err)
 
-        conn.query('SELECT * FROM categorias', (err, rows)=>{
-            if(err) return res.send(err)
+        conn.query('SELECT * FROM categorias', (err, rows) => {
+            if (err) return res.send(err)
             res.json(rows)
         })
     })
 })
 
 
+//RUTAS DE PRODUCTOS
+
 //ruta para borrar un producto
 
-app.delete('/Productos/:id', (req, res)=>{
-    req.getConnection((err, conn)=>{
-        if(err) return res.send(err)
+app.delete('/Productos/:id', (req, res) => {
+    req.getConnection((err, conn) => {
+        if (err) return res.send(err)
 
-        conn.query('DELETE FROM productos WHERE producto_id = ?', [req.params.id],(err, rows)=>{
-            if(err) return res.send(err)
+        conn.query('DELETE FROM productos WHERE producto_id = ?', [req.params.id], (err, rows) => {
+            if (err) return res.send(err)
 
             res.send(rows)
         })
@@ -97,36 +103,52 @@ app.delete('/Productos/:id', (req, res)=>{
 
 //ruta para crear un producto
 
-app.post('/Productos', (req, res)=>{
-    req.getConnection((err, conn)=>{
-        if(err) return res.send(err)
+app.post('/Productos', (req, res) => {
+    req.getConnection((err, conn) => {
+        if (err) return res.send(err)
 
-        conn.query('INSERT INTO productos set ?', [req.body],(err, rows)=>{
-            if(err) return res.send(err)
+        conn.query('INSERT INTO productos set ?', [req.body], (err, rows) => {
+            if (err) return res.send(err)
 
             res.send(rows)
         })
     })
 })
 
-//ruta para verificar cuenta
+//ruta para modificar un producto
 
-app.post('/api/login', (req, res)=>{
-    const {username, password} = req.body
+app.put('/Productos/:id', (req, res) => {
+    req.getConnection((err, conn) => {
+        if (err) return res.send(err)
+
+        conn.query('UPDATE productos set ? WHERE id = ?', [req.body, req.params.id], (err, rows) => {
+            if (err) return res.send(err)
+
+            res.send(rows)
+        })
+    })
+})
+
+
+
+//RUTA PARA VERIFICAR CUENTA
+
+app.post('/api/login', (req, res) => {
+    const { username, password } = req.body
     const values = [username, password]
 
-    req.getConnection((err, conn)=>{
-        if(err) return res.send(err)
+    req.getConnection((err, conn) => {
+        if (err) return res.send(err)
 
-        conn.query('SELECT * FROM usuarios WHERE username = ? AND password= ?', values, (err, result)=>{
-            if(err) {
+        conn.query('SELECT * FROM usuarios WHERE username = ? AND password= ?', values, (err, result) => {
+            if (err) {
                 console.log('Error en la consulta', err)
                 res.status(500).send(err)
-            }else{
+            } else {
                 console.log('Resultados de la consulta:', result)
-                if(result.length > 0){
+                if (result.length > 0) {
                     res.status(200).send("Usuario correcto")
-                }else{
+                } else {
                     res.status(400).send("Usuario no existe")
                 }
             }
@@ -135,7 +157,7 @@ app.post('/api/login', (req, res)=>{
 })
 
 
-app.listen(app.get('port'), ()=>{
+app.listen(app.get('port'), () => {
     console.log('server corriendo en el puerto', app.get('port'))
 })
 
